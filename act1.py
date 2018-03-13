449d552ed4c5a3ee17ae14f9667445b65f2ba81a
Ignacia <ibaeza1@uc.cl>
2018-03-13 14:11:10 -0300

    actividad avanzada

--- act1.py
+++ act1.py
@@ -1,69 +1,109 @@
-class Comuna(Casa,Edificio):
-    def __init__(self,nombre,lista_edificios ,lista_casas):
-        self.nombre = nombre
-        self.lista_edificios = lista_edificios
-        self.lista_casas = lista_casas
-    def agregar_casa(self,casa):
-        self.lista_casas.append(casa)
-        return self.lista_casas
-    def agregar_edificio(self,edificio):
-        self.lista_edificios.append(edificio)
-        return self.lista_edificios
-    def mostrar_lista(self):
-        for i in lista_edificios:
-            print(i)
-        for j in lista_casas:
-            print(j)
+            
 
-class Casa:
-    def __init__(self,direc,num):
-        self.direc = direc
-        self.ed = False
-        self.num = num
-    def electro_dependiente(self,el):
-        if el > 5000:
-            print('Es electrodependiente')
-        else:
-            print('No es electrodependiente')
+#class Medidor:
+ #   def __init__(self):
+
+#      self._kwh = None
+ #   @property
+  #  def kwh(self):
+   #     return self._kwh
+ #   @kwh.setter
+  #  def kwh(self,kwh):
+   #     self._kwh = kwh
+    #    print(self.kwh)
+
+class Cliente:
+    def __init__(self,nombre,rut):
+        self.nombre = nombre
+        self.rut = rut
+
+    def mostrar(self):
+        return self.nombre , self.rut
+
     
-class Edificio:
-    def __init__(self,direccion,nombre,medidor,lista_deptos):
+class Medidor:
+    
+    def __init__(self,kwh):
+        self.kwh = kwh
+        
+    def mostrar_kwh(self):
+        return int(self.kwh)
+    
+        
+class Edificio(Departamento):
+    
+    def __init__(self,direccion,nombre,kwh,medidor):
+        super().__init__(kwh)
         self.direccion = direccion
         self.nombre = nombre
-        self.medidor = medidor
-        self.lista_deptos = lista_deptos
+        
     def ed(self):
-        if int(self.medidor) > 10000:
+        if self.kwh > 10000:
             print(True)
         else:
             print(False)
+
             
-class Medidor:
-    def __init__(self):
-        self._kwh = None
-    @property
-    def kwh(self):
-        return self._kwh
-    @kwh.setter
-    def kwh(self,kwh):
-        self._kwh = kwh
+class Casa:
+    
+    def __init__(self,direc,num,el):
+        self.direc = direc
+        self.ed = False
+        self.num = num
+        self.el = int(el)
         
-class Departamento(Medidor):
-    def __init__(self,nombre,kwh):
+    def electro_dependiente(self):
+        if self.el > 5000:
+            return True
+        else:
+            return False
+
+        
+class Comuna(Casa):
+    
+    def __init__(self,nombre,direc,num,el):
         self.nombre = nombre
-        self.kwh = kwh
- 
+        super().__init__(direc,num,el)
+        self.lista_casas = []
+        
+    def agregar_casa(self):
+        if self.electro_dependiente() == True:
+            self.lista_casas.append(self.direc)
+        else:
+            pass
+        print(self.lista_casas)
+    def agregar_edificio(self):
+        pass
+
+
+class Departamento(Medidor,Cliente):
+    
+    def __init__(self,nombre,kwh):
+        super().__init__(kwh)
+        self.nombre = nombre
+                     
     def es_electrodp(self):   
         if self.kwh > 4000:
             print('{} es electrodependiente'.format(self.nombre))
         else:
             print('{} no es electrodependiente'.format(self.nombre))
+            
 
-med = Medidor()
-a = int(input('medidor:'))
-med.kwh = a
-depto = Departamento('A',med.kwh)
-depto.es_electrodp()
+#perro = Comuna('hola','213','2312',123123)
+#perro.agregar_casa()
+#med = Edificio('gh','A',a)
+i = 0
+
+while i <= 4:
+    a = int(input('Kwh:'))
+    b = input('direc:')
+    perro = Comuna('hola',b,'2312',a)
+    perro.agregar_casa()
+    i += 1
+    
+    
+
+
 
 
 
